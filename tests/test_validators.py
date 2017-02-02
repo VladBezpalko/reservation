17c42b1db5ed30fc15974bd3659c3b512b5cfff1
Vlad Bezpalko <vl.bezpalko@gmail.com>
2017-02-02 15:01:22 +0200

    Change test_validators

--- tests/test_validators.py
+++ tests/test_validators.py
@@ -10,31 +10,37 @@ from reservation.validators import (
 )
 
 
-def test_validate_not_it_past(fixt_date_in_the_past):
+def test_validate_not_in_past(fixt_normal_date):
+    validate_not_in_past(fixt_normal_date)
+
+
+def test_validate_in_past(fixt_date_in_the_past):
     with pytest.raises(ValidationError):
         validate_not_in_past(fixt_date_in_the_past)
 
 
-def test_validate_not_late(fixt_date_late_to_reserve):
+def test_validate_not_late(fixt_normal_date):
+    validate_not_late(fixt_normal_date)
 
+
+def test_validate_late(fixt_date_late_to_reserve):
     with pytest.raises(ValidationError):
         validate_not_late(fixt_date_late_to_reserve)
 
 
-def test_validate_not_weekend(fixt_date_weekend):
+def test_validate_not_weekend(fixt_normal_date):
+    validate_not_weekend(fixt_normal_date)
+
+
+def test_validate_weekend(fixt_date_weekend):
     with pytest.raises(ValidationError):
         validate_not_weekend(fixt_date_weekend)
+
+
+def test_validate_non_working_hours(fixt_normal_date):
+    validate_working_hours(fixt_normal_date)
 
 
 def test_validate_working_hours(fixt_date_after_work):
     with pytest.raises(ValidationError):
         validate_working_hours(fixt_date_after_work)
-
-
-def test_all_validators_accept_valid_date(fixt_normal_date):
-    validate_not_late(fixt_normal_date)
-    validate_not_in_past(fixt_normal_date)
-    validate_working_hours(fixt_normal_date)
-    validate_not_weekend(fixt_normal_date)
-
-    assert 1
